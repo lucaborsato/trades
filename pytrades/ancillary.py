updated adaptive stepsize of the numerical integrator, updated notebook for pyDE visualisation, and fixed bug for plot source names.

pytrades/ancillary.py
@@ -66,6 +66,8 @@ eps32bit = np.finfo(np.float32(1.0)).eps
 # sigma pos        0      1     2       3      4       5      6       7
 percentile_val = [68.27, 50.0, 15.865, 84.135, 2.265, 97.735, 0.135, 99.865]
 
+credible_val = [0.6827, 0.9544, 0.9974]
+
 parameters_folders = [
     "0000_sim_initial",
     "0004_sim_pso",
@@ -4724,7 +4726,8 @@ def calculate_hpd(trace):
     # cred = 0.9544 <-> -/+ 2 sigma
     # cred = 0.9974 <-> -/+ 3 sigma
 
-    credval = [0.6827, 0.9544, 0.9974]
+    # credval = [0.6827, 0.9544, 0.9974]
+    credval = credible_val
     hdi = [hpd(trace, cred=c) for c in credval]
 
     return hdi
@@ -4960,24 +4963,31 @@ def sqrte_to_e_parameters(fitting_in, names_par):
 
 
 # ==============================================================================
-def check_wrapped_parameters(names_par):
+def check_wrapped_parameters(names_par, bounds_par):
 
-    nfit = len(names_par)
-    wrapped = [False] * nfit
+    # nfit = len(names_par)
+    # wrapped = [False] * nfit
+
+    # for ifit, name in enumerate(names_par):
+    #     if (
+    #         # ("cos" in name)
+    #         # or ("sin" in name)
+    #         # or
+    #         ("lambda" in name)
+    #         or (name[0] == "w")
+    #         or ("mA" in name)
+    #     ):
+    #         wrapped[ifit] = True
 
     for ifit, name in enumerate(names_par):
-        if (
-            # ("cos" in name)
-            # or ("sin" in name)
-            # or
-            ("lambda" in name)
-            or (name[0] == "w")
-            or ("mA" in name)
-        ):
-            wrapped[ifit] = True
+        dbd = np.ptp(bounds_par[ifit,:])
+        if ('mA' in name) or (name[0] == 'w') or ('lN' in name) or ('lambda' in name):
+            if dbd == 360.0:
+                wrapped.append(ifit)
+        elif ('cos' in name) or ('sin' in name):
+            wrapped.append(ifit)
 
     return wrapped
-
 
 # ==============================================================================
 

pytrades/plot_oc.py
@@ -446,7 +446,14 @@ def plot_oc_T41(
     u_src = np.unique(sim.sources)
     # n_src = len(u_src)
     # ocolors = anc.set_colors(n_src, colormap=cli.color_map)
-    n_idsrc = np.max(list(cli.idsource_name.keys()))
+    if isinstance(cli.idsource_name, dict):
+        n_idsrc = np.max(list(cli.idsource_name.keys()))
+    elif isinstance(cli.idsource_name, (list, np.ndarray)):
+        n_idsrc = len(cli.idsource_name)
+        cli.idsource_name = {i+1: val for i, val in enumerate(cli.idsource_name)}
+    else:
+        cli.idsource_name = {1: "observations"}
+        n_idsrc = 1
     
     if isinstance(cli.color_map, dict):
         # full_colors = cli.color_map

pytrades/tests/trades_oc_gls.py
@@ -163,22 +163,22 @@ def get_args():
         cli.body_id = 2
 
     try:
-        cli.t_start = np.float64(cli.t_start)
+        cli.t_start = float(cli.t_start)
     except:
         cli.t_start = 0.0
 
     try:
-        cli.t_end = np.float64(cli.t_end)
+        cli.t_end = float(cli.t_end)
     except:
         cli.t_end = 365.25
 
     try:
-        cli.tref = np.float64(cli.tref)
+        cli.tref = float(cli.tref)
     except:
         cli.tref = None
 
     try:
-        cli.pref = np.float64(cli.pref)
+        cli.pref = float(cli.pref)
     except:
         cli.pref = None
 

pytrades/to_be_discarded/confidence_intervals_bck.py
@@ -33,7 +33,7 @@ def save_models_from_parameters(
     if trades_type:
         trades_fit_par, trades_names_par = fit_par_in, names_par_in
         gr.create_dataset(
-            "trades_par", data=trades_fit_par, dtype=np.float64, compression="gzip"
+            "trades_par", data=trades_fit_par, dtype=float, compression="gzip"
         )
         gr.create_dataset(
             "trades_names_par", data=trades_names_par, dtype="S20", compression="gzip"
@@ -42,20 +42,20 @@ def save_models_from_parameters(
         names_par = anc.emcee_names_to_trades(trades_names_par)
         fit_par = anc.e_to_sqrte_fitting(trades_fit_par, trades_names_par)
         gr.create_dataset("fit_par", data=fit_par,
-                          dtype=np.float64, compression="gzip")
+                          dtype=float, compression="gzip")
         gr.create_dataset("names_par", data=names_par,
                           dtype="S20", compression="gzip")
     else:
         fit_par, names_par = fit_par_in, names_par_in
         gr.create_dataset("fit_par", data=fit_par,
-                          dtype=np.float64, compression="gzip")
+                          dtype=float, compression="gzip")
         gr.create_dataset("names_par", data=names_par,
                           dtype="S20", compression="gzip")
         trades_fit_par = fit_par
         # trades_fit_par = anc.sqrte_to_e_fitting(fit_par_in, names_par_in)
         # trades_names_par = anc.emcee_names_to_trades(names_par_in)
         # gr.create_dataset(
-        #     "trades_par", data=trades_fit_par, dtype=np.float64, compression="gzip"
+        #     "trades_par", data=trades_fit_par, dtype=float, compression="gzip"
         # )
         # gr.create_dataset(
         #     "trades_names_par", data=trades_names_par, dtype="S20", compression="gzip"
@@ -85,8 +85,8 @@ def save_models_from_parameters(
     id_rv = np.argsort(time_rvx)
     time_rv, rv = time_rvx[id_rv], rvx[id_rv]
     gr.create_dataset("time_rv_mod", data=time_rv,
-                      dtype=np.float64, compression="gzip")
-    gr.create_dataset("rv_mod", data=rv, dtype=np.float64, compression="gzip")
+                      dtype=float, compression="gzip")
+    gr.create_dataset("rv_mod", data=rv, dtype=float, compression="gzip")
 
     stats_ttra = np.array(stats_ttra).astype(bool)
     for ipl in range(2, NB + 1):
@@ -100,10 +100,10 @@ def save_models_from_parameters(
         ttra = ttra[idx_tra]
         dur = dur[idx_tra]
         gr.create_dataset(
-            "TTs_%d" % (ipl), data=ttra, dtype=np.float64, compression="gzip"
+            "TTs_%d" % (ipl), data=ttra, dtype=float, compression="gzip"
         )
         gr.create_dataset(
-            "T41s_%d" % (ipl), data=dur, dtype=np.float64, compression="gzip"
+            "T41s_%d" % (ipl), data=dur, dtype=float, compression="gzip"
         )
     return
 
@@ -269,9 +269,9 @@ def main():
     posterior_file = os.path.join(cli.full_path, "posterior.hdf5")
     p_h5f = h5py.File(posterior_file, "w")
     p_h5f.create_dataset(
-        "posterior", data=flatchain_posterior, dtype=np.float64)
+        "posterior", data=flatchain_posterior, dtype=float)
     p_h5f.create_dataset(
-        "loglikelihood", data=lnprob_burnin.reshape((-1)), dtype=np.float64
+        "loglikelihood", data=lnprob_burnin.reshape((-1)), dtype=float
     )
     p_h5f["posterior"].attrs["nfit"] = nfit
     p_h5f["posterior"].attrs["nposterior"] = np.shape(flatchain_posterior)[0]
@@ -519,7 +519,7 @@ def main():
             s_h5f.create_dataset(
                 "parameters/{:s}/fitted/parameters".format(s_id_sim),
                 data=parameters,
-                dtype=np.float64,
+                dtype=float,
                 compression="gzip",
             )
             s_h5f.create_dataset(
@@ -537,7 +537,7 @@ def main():
             s_h5f.create_dataset(
                 "parameters/{:s}/fitted/sigma".format(s_id_sim),
                 data=sigma_par.T,
-                dtype=np.float64,
+                dtype=float,
                 compression="gzip",
             )
             s_h5f["parameters/{:s}/fitted/sigma".format(s_id_sim)].attrs[
@@ -547,7 +547,7 @@ def main():
             s_h5f.create_dataset(
                 "parameters/{:s}/derived/parameters".format(s_id_sim),
                 data=derived_par,
-                dtype=np.float64,
+                dtype=float,
                 compression="gzip",
             )
             s_h5f.create_dataset(
@@ -565,7 +565,7 @@ def main():
             s_h5f.create_dataset(
                 "parameters/{:s}/derived/sigma".format(s_id_sim),
                 data=sigma_derived.T,
-                dtype=np.float64,
+                dtype=float,
                 compression="gzip",
             )
             s_h5f["parameters/{:s}/derived/sigma".format(s_id_sim)].attrs[
@@ -659,7 +659,7 @@ def main():
     s_h5f.create_dataset(
         "confidence_intervals/fitted/ci",
         data=ci_fitted.T,
-        dtype=np.float64,
+        dtype=float,
         compression="gzip",
     )
     s_h5f.create_dataset(
@@ -677,7 +677,7 @@ def main():
     s_h5f.create_dataset(
         "confidence_intervals/fitted/percentiles",
         data=np.array(anc.percentile_val[2:]),
-        dtype=np.float64,
+        dtype=float,
         compression="gzip",
     )  # now it not true...
 
@@ -795,7 +795,7 @@ def main():
         compression="gzip",
     )
     h5f.create_dataset(
-        "derived_posterior", data=der_posterior, dtype=np.float64, compression="gzip"
+        "derived_posterior", data=der_posterior, dtype=float, compression="gzip"
     )
     h5f.create_dataset(
         "units_derived",
@@ -817,7 +817,7 @@ def main():
     s_h5f.create_dataset(
         "confidence_intervals/derived/ci",
         data=ci_derived.T,
-        dtype=np.float64,
+        dtype=float,
         compression="gzip",
     )
     s_h5f.create_dataset(
@@ -835,7 +835,7 @@ def main():
     s_h5f.create_dataset(
         "confidence_intervals/derived/percentiles",
         data=np.array(anc.percentile_val[2:]),
-        dtype=np.float64,
+        dtype=float,
         compression="gzip",
     )
     # ==============================================================================

pytrades/to_be_discarded/trades_emcee_sqrte.py
@@ -331,7 +331,7 @@ def lnprob(fitting_parameters):
     loglhd = 0.0
     check = 1
     loglhd, logprior, check = pytrades.fortran_logprob(
-        np.asarray(fitting_parameters, dtype=np.float64)
+        np.asarray(fitting_parameters, dtype=float)
     )
     # not needed to add ln_err_const, because it is included in the loglhd now
     # loglhd = loglhd + ln_err_const # ln_err_const: global variable
@@ -547,18 +547,18 @@ if nsave != False:
     f_hdf5.create_dataset(
         "parameter_names", data=anc.encode_list(parameter_names), dtype="S10"
     )
-    f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=np.float64)
-    f_hdf5.create_dataset("chains", (nwalkers, nruns, nfit), dtype=np.float64)
+    f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=float)
+    f_hdf5.create_dataset("chains", (nwalkers, nruns, nfit), dtype=float)
     f_hdf5["chains"].attrs["nwalkers"] = nwalkers
     f_hdf5["chains"].attrs["nruns"] = nruns
     f_hdf5["chains"].attrs["nfit"] = nfit
     f_hdf5["chains"].attrs["nfree"] = nfree
-    f_hdf5.create_dataset("lnprobability", (nwalkers, nruns), dtype=np.float64)
+    f_hdf5.create_dataset("lnprobability", (nwalkers, nruns), dtype=float)
     # f_hdf5['lnprobability'].attrs['ln_err_const'] = ln_err_const
     f_hdf5.create_dataset(
-        "acceptance_fraction", data=np.zeros((nfit)), dtype=np.float64
+        "acceptance_fraction", data=np.zeros((nfit)), dtype=float
     )
-    f_hdf5.create_dataset("autocor_time", data=np.zeros((nfit)), dtype=np.float64)
+    f_hdf5.create_dataset("autocor_time", data=np.zeros((nfit)), dtype=float)
     f_hdf5.close()
 
     pos = p0
@@ -626,7 +626,7 @@ else:
 
     # save chains with original shape as hdf5 file
     f_hdf5 = h5py.File(os.path.join(working_folder, "emcee_summary.hdf5"), "w")
-    f_hdf5.create_dataset("chains", data=sampler.chain, dtype=np.float64)
+    f_hdf5.create_dataset("chains", data=sampler.chain, dtype=float)
     f_hdf5["chains"].attrs["nwalkers"] = nwalkers
     f_hdf5["chains"].attrs["nruns"] = nruns
     f_hdf5["chains"].attrs["nfit"] = nfit
@@ -635,15 +635,15 @@ else:
     f_hdf5.create_dataset(
         "parameter_names", data=anc.encode_list(parameter_names), dtype="S10"
     )
-    f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=np.float64)
+    f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=float)
     f_hdf5.create_dataset(
-        "acceptance_fraction", data=sampler.acceptance_fraction, dtype=np.float64
+        "acceptance_fraction", data=sampler.acceptance_fraction, dtype=float
     )
     f_hdf5["acceptance_fraction"].attrs[
         "mean_acceptance_fraction"
     ] = mean_acceptance_fraction
-    f_hdf5.create_dataset("autocor_time", data=acor_time, dtype=np.float64)
-    f_hdf5.create_dataset("lnprobability", data=lnprobability, dtype=np.float64)
+    f_hdf5.create_dataset("autocor_time", data=acor_time, dtype=float)
+    f_hdf5.create_dataset("lnprobability", data=lnprobability, dtype=float)
     # f_hdf5['lnprobability'].attrs['ln_err_const'] = ln_err_const
     f_hdf5.close()
 

pytrades/to_be_discarded/trades_pso2emcee_sqrte.py
@@ -307,10 +307,10 @@ def lnprob(fitting_parameters):
     loglhd = 0.0
     check = 1
     # loglhd, check = f90trades.fortran_loglikelihood(
-    #     np.asarray(fitting_parameters, dtype=np.float64)
+    #     np.asarray(fitting_parameters, dtype=float)
     # )
     loglhd, logprior, check = f90trades.fortran_logprob(
-        np.asarray(fitting_parameters, dtype=np.float64)
+        np.asarray(fitting_parameters, dtype=float)
     )
     # not needed to add ln_err_const, because it is included in the loglhd now
     # loglhd = loglhd + ln_err_const # ln_err_const: global variable
@@ -391,12 +391,12 @@ for iter_global in range(0, n_global):
         anc.print_both(" completed run_pso", of_run)
 
         pso_best_evolution = np.asarray(
-            f90trades.pso_best_evolution[...], dtype=np.float64
+            f90trades.pso_best_evolution[...], dtype=float
         )
         anc.print_both(" pso_best_evolution retrieved", of_run)
 
         anc.print_both(" last pso_best_evolution", of_run)
-        last_pso_fitness = pso_best_evolution[-1, -1].astype(np.float64)
+        last_pso_fitness = pso_best_evolution[-1, -1].astype(float)
         anc.print_both(" fitness = {}".format(last_pso_fitness), of_run)
 
         # SAVE PSO SIMULATION IN pso_run.hdf5 FILE
@@ -405,20 +405,20 @@ for iter_global in range(0, n_global):
         )
         pso_hdf5 = h5py.File(os.path.join(pso_path, "pso_run.hdf5"), "w")
         pso_hdf5.create_dataset(
-            "population", data=f90trades.population, dtype=np.float64
+            "population", data=f90trades.population, dtype=float
         )
         pso_hdf5.create_dataset(
-            "population_fitness", data=f90trades.population_fitness, dtype=np.float64
+            "population_fitness", data=f90trades.population_fitness, dtype=float
         )
-        pso_hdf5.create_dataset("pso_parameters", data=pso_parameters, dtype=np.float64)
+        pso_hdf5.create_dataset("pso_parameters", data=pso_parameters, dtype=float)
         pso_hdf5.create_dataset(
-            "pso_fitness", data=np.array(pso_fitness), dtype=np.float64
-        )
-        pso_hdf5.create_dataset(
-            "pso_best_evolution", data=pso_best_evolution, dtype=np.float64
+            "pso_fitness", data=np.array(pso_fitness), dtype=float
         )
         pso_hdf5.create_dataset(
-            "parameters_minmax", data=trades_minmax, dtype=np.float64
+            "pso_best_evolution", data=pso_best_evolution, dtype=float
+        )
+        pso_hdf5.create_dataset(
+            "parameters_minmax", data=trades_minmax, dtype=float
         )
         pso_hdf5.create_dataset(
             "parameter_names", data=anc.encode_list(trades_names), dtype="S10"
@@ -584,18 +584,18 @@ for iter_global in range(0, n_global):
         f_hdf5.create_dataset(
             "parameter_names", data=anc.encode_list(parameter_names), dtype="S10"
         )
-        f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=np.float64)
-        f_hdf5.create_dataset("chains", (nwalkers, nruns, nfit), dtype=np.float64)
+        f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=float)
+        f_hdf5.create_dataset("chains", (nwalkers, nruns, nfit), dtype=float)
         f_hdf5["chains"].attrs["nwalkers"] = nwalkers
         f_hdf5["chains"].attrs["nruns"] = nruns
         f_hdf5["chains"].attrs["nfit"] = nfit
         f_hdf5["chains"].attrs["nfree"] = nfree
-        f_hdf5.create_dataset("lnprobability", (nwalkers, nruns), dtype=np.float64)
+        f_hdf5.create_dataset("lnprobability", (nwalkers, nruns), dtype=float)
         # f_hdf5['lnprobability'].attrs['ln_err_const'] = ln_err_const
         f_hdf5.create_dataset(
-            "acceptance_fraction", data=np.zeros((nfit)), dtype=np.float64
+            "acceptance_fraction", data=np.zeros((nfit)), dtype=float
         )
-        f_hdf5.create_dataset("autocor_time", data=np.zeros((nfit)), dtype=np.float64)
+        f_hdf5.create_dataset("autocor_time", data=np.zeros((nfit)), dtype=float)
         f_hdf5.close()
 
         pos = p0
@@ -662,7 +662,7 @@ for iter_global in range(0, n_global):
 
         # save chains with original shape as hdf5 file
         f_hdf5 = h5py.File(os.path.join(pso_path, "emcee_summary.hdf5"), "w")
-        f_hdf5.create_dataset("chains", data=sampler.chain, dtype=np.float64)
+        f_hdf5.create_dataset("chains", data=sampler.chain, dtype=float)
         f_hdf5["chains"].attrs["nwalkers"] = nwalkers
         f_hdf5["chains"].attrs["nruns"] = nruns
         f_hdf5["chains"].attrs["nfit"] = nfit
@@ -671,15 +671,15 @@ for iter_global in range(0, n_global):
         f_hdf5.create_dataset(
             "parameter_names", data=anc.encode_list(parameter_names), dtype="S10"
         )
-        f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=np.float64)
+        f_hdf5.create_dataset("boundaries", data=parameters_minmax, dtype=float)
         f_hdf5.create_dataset(
-            "acceptance_fraction", data=sampler.acceptance_fraction, dtype=np.float64
+            "acceptance_fraction", data=sampler.acceptance_fraction, dtype=float
         )
         f_hdf5["acceptance_fraction"].attrs[
             "mean_acceptance_fraction"
         ] = mean_acceptance_fraction
-        f_hdf5.create_dataset("autocor_time", data=acor_time, dtype=np.float64)
-        f_hdf5.create_dataset("lnprobability", data=lnprobability, dtype=np.float64)
+        f_hdf5.create_dataset("autocor_time", data=acor_time, dtype=float)
+        f_hdf5.create_dataset("lnprobability", data=lnprobability, dtype=float)
         # f_hdf5['lnprobability'].attrs['ln_err_const'] = ln_err_const
         f_hdf5.close()
 

pytrades/to_be_discarded/trades_python_test_read_data_v1.py
@@ -14,10 +14,10 @@ import constants as cst
 
 def main():
     # integration parameters
-    t_start = np.float64(2454965.0)
-    t_epoch = np.float64(2455088.212)
-    t_int = np.float64(500.0)
-    step_in = np.float64(1.0e-3)
+    t_start = float(2454965.0)
+    t_epoch = float(2455088.212)
+    t_int = float(500.0)
+    step_in = float(1.0e-3)
 
     print("set integration parameters")
 